ca29fd67a8ebb93e2b454c9fe5d199069ad67cf9
Farhan Tanvir <farhantanvir65@gmail.com>
2022-01-20 15:46:41 +0900

    ModifyAutoDeploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello CI cd
+          Hello CI cd V2
         </p>
         <a
           className="App-link"
