bcd9785423396392eb82a6c146194e8f6b806311
Farhan Tanvir <farhantanvir65@gmail.com>
2022-01-25 16:25:02 +0900

    Update AutoDeploy.yml

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import congrats from './congratulation.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello CI cd V2 WTF
+        <img src={congrats} className="App-logo" alt="logo" />
+        <p className="App-text">
+          Fun . isn't it ?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
